7e5d0b96364799ac94109b744018df23d5788116
MrPeche <Gitgudman1>
2020-03-24 17:04:54 -0500

    Creacion de registro uno ... 5

--- app/src/main/java/com/example/neolink_app/registrotwo.java
+++ app/src/main/java/com/example/neolink_app/registrotwo.java
@@ -2,6 +2,7 @@ package com.example.neolink_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,8 +13,16 @@ public class registrotwo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrotwo);
     }
+    public void botonverificarcodigo(View view){
 
+    }
     public void siguiente(View view){
-        //Intent i = new Intent(this,)
+        Intent i = new Intent(this,registrothree.class);
+        startActivity(i);
+    }
+    public void volver(View view){
+        Intent i = new Intent(this,MainActivity.class);
+        i.setFlags(i.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 }
