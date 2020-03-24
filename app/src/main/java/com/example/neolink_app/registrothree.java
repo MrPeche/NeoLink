d6954d5e94046ed41ef5a9d0144f63221dca8157
MrPeche <Gitgudman1>
2020-03-24 18:05:14 -0500

    Creacion de registro uno ... 6

--- app/src/main/java/com/example/neolink_app/registrothree.java
+++ app/src/main/java/com/example/neolink_app/registrothree.java
@@ -2,13 +2,47 @@ package com.example.neolink_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class registrothree extends AppCompatActivity {
+
+    private TextInputEditText ticket;
+    private TextInputLayout layticket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrothree);
+        ticket = findViewById(R.id.ticketrecuperarthree);
+        layticket = findViewById(R.id.layoutcodigoregistrothree);
+    }
+
+    public boolean validarticket(String ticket){
+        return true;
+    }
+
+    public void siguiente(View view){
+        if(validarticket(ticket.getText().toString())){
+            //Terminar mensaje y irnos al main
+            Toast.makeText( this, "Registro completado", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, MainActivity.class);
+            i.setFlags(i.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        } else Toast.makeText( this, "Ticket invalido", Toast.LENGTH_SHORT).show();
+
+        Intent i = new Intent(this,registrothree.class);
+        startActivity(i);
+    }
+
+    public void volver(View view){
+        Intent i = new Intent(this,MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/neolink_app/registrotwo.java
+++ app/src/main/java/com/example/neolink_app/registrotwo.java
@@ -5,24 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class registrotwo extends AppCompatActivity {
+
+    TextInputEditText codigo;
+    TextInputLayout laycodigo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrotwo);
+        codigo = findViewById(R.id.codigorecuperartwo);
+        laycodigo = findViewById(R.id.layoutcodigoregistrotwo);
     }
-    public void botonverificarcodigo(View view){
+    public boolean verificarcodigo(String codigo){
+        return true;
+    }
 
-    }
     public void siguiente(View view){
-        Intent i = new Intent(this,registrothree.class);
-        startActivity(i);
+
+        if(verificarcodigo(codigo.getText().toString())) {
+            Intent i = new Intent(this, registrothree.class);
+            startActivity(i);
+        } else Toast.makeText( this, "Codigo Invalido", Toast.LENGTH_SHORT).show();
     }
     public void volver(View view){
         Intent i = new Intent(this,MainActivity.class);
-        i.setFlags(i.FLAG_ACTIVITY_CLEAR_TOP);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(i);
     }
 }
