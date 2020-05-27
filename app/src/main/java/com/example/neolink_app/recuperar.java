53e5ff55a1a29a9039a942267ff0592e10d253d5
MrPeche <Gitgudman1>
2020-05-27 12:17:06 -0500

    Recuperar Listo

--- app/src/main/java/com/example/neolink_app/recuperar.java
+++ app/src/main/java/com/example/neolink_app/recuperar.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,6 +19,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class recuperar extends AppCompatActivity {
     private TextInputEditText correo;
+    private Button botonR;
+    private TextView botonVR;
+    private ProgressBar loadR;
     private FirebaseAuth auth = FirebaseAuth.getInstance();
     //private Button recuboton;
 
@@ -24,21 +30,47 @@ public class recuperar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar);
         correo = (TextInputEditText)findViewById(R.id.correoRecuperar);
+        botonR = findViewById(R.id.button_recuperar);
+        botonVR = findViewById(R.id.volver);
+        loadR = findViewById(R.id.CargadoR);
     }
     public void BotonRecuperar(View view){ //Funcion para recuperar
         //verificar escritirua
-        if(correo.length() != 0){
+        if(correo.length() != 0) {
+            setitR();
             auth.sendPasswordResetEmail(correo.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
-                  if(task.isSuccessful()){
-                      logradaso();
-                  } else falladaso();
+                    if (task.isSuccessful()) {
+                        logradaso();
+                        finish();
+                    } else {
+                        try{
+                            throw task.getException();
+                        } catch (Exception e){
+                            Toast.makeText( recuperar.this, "Correo enviado con exito", Toast.LENGTH_SHORT).show();
+                        }
+                        falladaso();
+                        setitbackR();
+                    }
                 }
             });
-            finish();
-        } else falladaso();
+        }
         //Mandar a Correo
+    }
+
+    void setitR(){
+        botonR.setEnabled(false);
+        botonVR.setEnabled(false);
+        loadR.setVisibility(View.VISIBLE);
+        correo.setFocusable(false);
+    }
+
+    void setitbackR(){
+        botonR.setEnabled(true);
+        botonVR.setEnabled(true);
+        loadR.setVisibility(View.GONE);
+        correo.setFocusableInTouchMode(true);
     }
     public void logradaso(){
         Toast.makeText( this, "Correo enviado con exito", Toast.LENGTH_SHORT).show();
