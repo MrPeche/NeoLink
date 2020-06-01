a7eb5b3b23a2a48457e286960ebbdbc0382a158a
MrPeche <Gitgudman1>
2020-06-01 02:38:14 -0500

    añadiendo layers

---
+++ app/src/main/java/com/example/neolink_app/actividadbase.java
@@ -0,0 +1,14 @@
+package com.example.neolink_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actividadbase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividadbase);
+    }
+}
