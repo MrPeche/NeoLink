c77014c404374e012d50dc5243ebddee573f5cb6
MrPeche <Gitgudman1>
2020-06-02 20:29:40 -0500

    toy avanzando el drawer 1

--- app/src/main/java/com/example/neolink_app/datosgenerales.java
+++ app/src/main/java/com/example/neolink_app/datosgenerales.java
@@ -26,7 +26,7 @@ public class datosgenerales extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    
+
     private String mParam1;
     private String mParam2;
 
