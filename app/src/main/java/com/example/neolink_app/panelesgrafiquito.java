32212b189a0500f65884fcbf880cb4eac2bb170f
MrPeche <Gitgudman1>
2020-07-08 03:10:14 -0500

    trabajando en el calendario

--- app/src/main/java/com/example/neolink_app/panelesgrafiquito.java
+++ app/src/main/java/com/example/neolink_app/panelesgrafiquito.java
@@ -3,6 +3,7 @@ package com.example.neolink_app;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -15,9 +16,14 @@ import android.view.ViewGroup;
 import com.example.neolink_app.adaptadores.viewpagergrafiquitosAdapter;
 import com.example.neolink_app.clases.Horas;
 import com.example.neolink_app.viewmodels.MasterDrawerViewModel;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.DateValidatorPointBackward;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class panelesgrafiquito extends Fragment {
@@ -26,6 +32,7 @@ public class panelesgrafiquito extends Fragment {
     private MasterDrawerViewModel archi;
     private ViewPager2 vp;
     private viewpagergrafiquitosAdapter adapter;
+    private MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
 
     public panelesgrafiquito() {
         // Required empty public constructor
@@ -53,13 +60,20 @@ public class panelesgrafiquito extends Fragment {
         int hoymes = ahora.get(Calendar.MONTH)+1;
         int hoydia = ahora.get(Calendar.DAY_OF_MONTH);
 
+
         archi = new ViewModelProvider(getActivity()).get(MasterDrawerViewModel.class);
         vp = view.findViewById(R.id.viewpagergraficos);
         String nombre = "NL2006-0002";
         String sensor = "k";
         ArrayList<String> nodaso = new ArrayList<>();
         nodaso.add(nombre);
+
+        setmycalendarbruh();
+        builder.setCalendarConstraints(buildtheconstraint().build());
+
+        final MaterialDatePicker materialdatepick = builder.build();
         final ArrayList<String> nodito = nodaso;
+
         archi.getLivedaylydata(nombre,hoyaño,hoymes,hoydia,sensor);
         archi.datahoy.observe(getViewLifecycleOwner(), new Observer<Horas>(){
             @Override
@@ -68,12 +82,24 @@ public class panelesgrafiquito extends Fragment {
                 vp.setAdapter(adapter);
             }
         });
+
         ((actividadbase)getActivity()).fabcalendar();
         ((actividadbase)getActivity()).fabaparecer();
         ((actividadbase)getActivity()).fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                materialdatepick.show(getParentFragmentManager(), "DATE PICKER");
+            }
+        });
+        materialdatepick.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Pair<Long, Long>>() {
+            @Override
+            public void onPositiveButtonClick(Pair<Long, Long> selection) {
+                Calendar desde = traductordecalendario(selection.first);
+                Calendar hasta = traductordecalendario(selection.second);
+                int diaD = desde.get(Calendar.DAY_OF_MONTH);
+                int diaH = hasta.get(Calendar.DAY_OF_MONTH);
+                int mont1 = desde.get(Calendar.MONTH);
+                int mont2 = hasta.get(Calendar.MONTH);
             }
         });
 
@@ -82,5 +108,24 @@ public class panelesgrafiquito extends Fragment {
         super.onDestroy();
         ((actividadbase)getActivity()).fabdesparecer();
         ((actividadbase)getActivity()).fabplus();
+    }
+    private void setmycalendarbruh(){
+        long today = MaterialDatePicker.todayInUtcMilliseconds();
+        this.builder.setTheme(R.style.materialtheme);
+        this.builder.setTitleText("Seleccione el rango de días que quiere visualizar");
+        //this.builder.setSelection(today);
+    }
+    private CalendarConstraints.Builder buildtheconstraint(){
+        CalendarConstraints.Builder constraing = new CalendarConstraints.Builder();
+        constraing.setValidator(DateValidatorPointBackward.now());
+        Calendar iniciocalendario = GregorianCalendar.getInstance();
+        iniciocalendario.set(2020,1,1);
+        constraing.setStart(iniciocalendario.getTimeInMillis());
+        return constraing;
+    }
+    private Calendar traductordecalendario(long date){
+        Calendar resultado = Calendar.getInstance();
+        resultado.setTimeInMillis(date);
+        return resultado;
     }
 }
